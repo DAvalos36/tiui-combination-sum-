41c0354da9737106a5664617de2a1f72f1fd8483
DAvalos36 <davalos@itparral.edu.mx>
2024-07-03 03:07:34 -0600

    feat: backTracking implemented

--- src/index.js
+++ src/index.js
@@ -6,11 +6,36 @@
  * @param {number} startFrom - index of the candidate to start further exploration from.
  * @return {number[][]}
  */
-
 const combinationSumRecursive = (
-    
-  }
-  
+	candidates,
+	remainingSum,
+	finalCombinations,
+	currentCombination,
+	startFrom,
+) => {
+	if (remainingSum === 0) {
+		finalCombinations.push([...currentCombination]);
+		return;
+	}
+
+	for (let i = startFrom; i < candidates.length; i++) {
+		const currentNumber = candidates[i];
+		if (currentNumber <= remainingSum) {
+			currentCombination.push(currentNumber);
+			combinationSumRecursive(
+				candidates,
+				remainingSum - currentNumber,
+				finalCombinations,
+				currentCombination,
+				i,
+			);
+			currentCombination.pop();
+		}
+	}
+
+	return finalCombinations;
+};
+
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
    *
@@ -19,7 +44,16 @@ const combinationSumRecursive = (
    * @return {number[][]}
    */
 const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
-  }
+	const finalCombinations = [];
+	const currentCombination = [];
+	combinationSumRecursive(
+		candidates,
+		target,
+		finalCombinations,
+		currentCombination,
+		0,
+	);
+	return finalCombinations;
+};
 
 module.exports = combinationSum;
